647867f1480239fa85bac900106b7e00af6e7a8f
tejaattainu <tejspvg@gmail.com>
2020-08-21 11:04:02 +0530

    new navbar added

--- Frontend/home-seek/src/App.js
+++ Frontend/home-seek/src/App.js
@@ -1,32 +1,11 @@
 import React from 'react';
-import { Switch, Redirect, Route } from 'react-router-dom'
-import './App.css';
-import RegisterPage from './pages/RegisterPage';
-import Loginpage from './pages/LoginPage';
-import Homepage from './pages/Homepage';
 import Navbar from './components/Navbar';
-// import DetailPage from './pages/DetailPage';
-import ListingPage from './pages/ListingPage';
-import ChatPage from './pages/ChatPage'
-import ForgotPassword from './pages/ForgotPassword';
-import RevivePassword from './pages/RevivePassword';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Switch>
-        <Route exact path='/' component={Homepage}/>
-        <Route exact path='/signUp' component={RegisterPage}/>  
-        <Route exact path='/signIn' component={Loginpage}/>
-        <Route exact path='/forgotPassword' component={ForgotPassword}/>
-        <Route exact path='/revivePassword/:token' component={RevivePassword}/>
-        <Route exact path='/chat' component={ChatPage} />
-        <Route exact path='/owner/listing/create' component={ListingPage} />
-        <Redirect to='/' />  
-      </Switch>
-      {/* <DetailPage />
-      <ListingPage /> */}
+      <Navbar />
     </div>
   );
 }
--- Frontend/home-seek/src/components/Navbar.jsx
+++ Frontend/home-seek/src/components/Navbar.jsx
@@ -1,43 +1,130 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
-import { logoutUser } from '../redux/actions/userActions'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link, Switch, Route, Redirect } from 'react-router-dom';
+import { Layout, Menu, Button } from 'antd';
+import {
+	MenuUnfoldOutlined,
+	MenuFoldOutlined,
+	UserOutlined,
+	VideoCameraOutlined,
+	UploadOutlined
+} from '@ant-design/icons';
 
-const Navbar = ({logoutUser, user, history}) => {
-    const handleClick = (e) => {
-        e.preventDefault()
-        logoutUser()
-        return !user ? history.push('/signIn') : null
-    }
-    return (
-        <div className='navbar'>
-            <Link style={{textDecoration: 'none'}} to='/'>
-                    <img className='brand' alt='logo' src="https://img.icons8.com/wired/50/000000/home.png"/>
-            </Link>
-            <div className='user-regd'>
-                {
-                    !user || !user.token
-                        ? 
-                            <>
-                                <Link to='/signUp'><button>sign Up</button></Link>
-                                <Link to='/signIn'><button>sign In</button></Link>
-                            </>
-                        :   
-                            <>
-                                <button onClick ={handleClick}>sign Out</button>
-                                <Link to='/chat'><button>Chat</button></Link>
-                                <Link to='/owner/listing/create'><button>CreatePost</button></Link>
-                            </>
-                }
-            </div>
-        </div>
-    )
+import '../styles/navbar.css';
+import RegisterPage from '../pages/RegisterPage';
+import Loginpage from '../pages/Loginpage';
+import Homepage from '../pages/Homepage';
+// import DetailPage from './pages/DetailPage';
+import ListingPage from '../pages/ListingPage';
+import ChatPage from '../pages/ChatPage';
+import ForgotPassword from '../pages/ForgotPassword';
+import RevivePassword from '../pages/RevivePassword';
+import {logoutUser} from '../redux/actions/userActions';
+
+const { Header, Sider, Content } = Layout;
+
+class Navbar extends Component {
+	state = {
+		collapsed: false
+	};
+
+	toggle = () => {
+		this.setState({
+			collapsed: !this.state.collapsed
+		});
+	};
+
+	handleClick = () => {
+		this.props.logoutUser();
+	};
+	render() {
+		return (
+			<div>
+				<Layout>
+					<Sider
+						trigger={null}
+						collapsible
+						collapsed={this.state.collapsed}
+						style={{
+							overflow: 'auto',
+							height: '100vh',
+							position: 'fixed',
+							left: 0
+						}}
+					>
+						<div className="logo" />
+						<Menu theme="dark" mode="inline" defaultSelectedKeys={[ '1' ]}>
+							<Menu.Item key="1" icon={<UserOutlined />}>
+								<Link to="/">Home</Link>
+							</Menu.Item>
+							<Menu.Item key="2" icon={<VideoCameraOutlined />}>
+								<Link to="/owner/listing/create">Listings</Link>
+							</Menu.Item>
+							<Menu.Item key="3" icon={<UploadOutlined />}>
+								<Link to="/userrequests">User Request</Link>
+							</Menu.Item>
+							<Menu.Item key="4" icon={<UploadOutlined />}>
+								<Link to="/normalrequests">Normal Request</Link>
+							</Menu.Item>
+							<Menu.Item key="5" icon={<UploadOutlined />}>
+								<Link to="/profile">Profile</Link>
+							</Menu.Item>
+						</Menu>
+					</Sider>
+					<Layout className="site-layout" style={{ marginLeft: !this.state.collapsed ? 200 : 70 }}>
+						<Header className="site-layout-background" style={{ padding: 0 }}>
+							{React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+								className: 'trigger',
+								onClick: this.toggle
+							})}
+							{!this.props.user ? (
+								<>
+									<Link to="/signUp">
+										<Button style={{ marginLeft: 1350, marginRight: 15 }}>Register</Button>
+									</Link>
+									<Link to="/signIn">
+										<Button style={{ marginLeft: 25 }}>Login</Button>
+									</Link>
+								</>
+							) : (
+								<>
+									<Button style={{ marginLeft: 1350, marginRight: 15 }} onClick={this.handleClick}>
+										Logout
+									</Button>
+                                    <Link to="/chat">
+										<Button style={{ marginLeft: 25 }}>chat</Button>
+									</Link>
+								</>
+							)}
+						</Header>
+						<Content
+							className="site-layout-background"
+							style={{
+								margin: '24px 16px',
+								padding: 24,
+								minHeight: 280
+							}}
+						>
+							<Switch>
+								<Route exact path="/" component={Homepage} />
+								<Route exact path="/signUp" component={RegisterPage} />
+								<Route exact path="/signIn" component={Loginpage} />
+								<Route exact path="/forgotPassword" component={ForgotPassword} />
+								<Route exact path="/revivePassword/:token" component={RevivePassword} />
+								<Route exact path="/chat" component={ChatPage} />
+								<Route exact path="/owner/listing/create" component={ListingPage} />
+								<Redirect to="/" />
+							</Switch>
+						</Content>
+					</Layout>
+				</Layout>
+			</div>
+		);
+	}
 }
 
-const mapStateToProps = state => {
-    return {
-        user : state.userState.user
-    }
-}
+const mapStateToProps = (storeState) => {
+	return { user: storeState.userState.user };
+};
 
-export default connect(mapStateToProps, { logoutUser })(Navbar)
+export default connect(mapStateToProps,{logoutUser})(Navbar);
